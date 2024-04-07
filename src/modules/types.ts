6007c2baaafc09a0b231a385ef266acd879aea87
meohyun2 <eogus6242@gmail.com>
2024-04-08 01:52:06 +0900

    add: transaction 페이지 추가

---
+++ src/modules/types.ts
@@ -0,0 +1,17 @@
+import { StaticImageData } from "next/image";
+
+export interface Exchange {
+  name: string;
+  logo: StaticImageData;
+}
+
+export interface Token {
+  symbol: string;
+  name: string;
+  logo: StaticImageData;
+  price: number;
+}
+
+export type ExchangeType = "binance" | "bithumb";
+export type TokenType = "BTC" | "ETH" | "USDT" | "USDC";
+export type TransactionType = "deposit" | "withdraw" | "swap";
