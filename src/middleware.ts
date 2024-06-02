19db73ccf7db85ddd169e6fd217533d6d331b6f9
meohyun2 <eogus6242@gmail.com>
2024-06-03 03:45:56 +0900

    fix: upgrade-insecure-requests 적용

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,12 @@ export async function middleware(request: NextRequest) {
   const { nextUrl, cookies } = request;
   const url = nextUrl.clone();
   const sessionCookie = cookies.get("JSESSIONID");
-  const { validUser } = await checkAuth(`${sessionCookie?.name}=${sessionCookie?.value}`);
+  const { validUser } = await checkAuth(
+    `${sessionCookie?.name}=${sessionCookie?.value}`
+  );
+  const requestHeaders = new Headers(request.headers);
+
+  requestHeaders.set("Content-Security-Policy", "upgrade-insecure-requests");
 
   if (AUTH_PAGES.some((page) => nextUrl.pathname.startsWith(page))) {
     if (!validUser) {
