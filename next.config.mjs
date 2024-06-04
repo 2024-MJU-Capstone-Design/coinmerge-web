6b44904d68c7161620a1ee29eafed4e0db66daad
meohyun2 <eogus6242@gmail.com>
2024-06-05 05:45:55 +0900

    fix: 헤더 옵션 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source :'/:path',
+        source: '/(.*)',
         headers: [
           {
             key: 'Strict-Transport-Security',
